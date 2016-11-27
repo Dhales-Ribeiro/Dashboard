b68688cbc570ec35d475a2cb109c9305c3c88aac
Dhales Ribeiro <wm.dhales@gmail.com>
2016-11-27 08:39:45 -0200

    - Configurando cores do sistema

--- config/app.js
+++ config/app.js
@@ -3,4 +3,25 @@
  */
 
 
-var application = angular.module("dashboard", [ "ui.router", 'ngMaterial', 'LocalStorageModule']);
+var application = angular.module("dashboard", [ "ui.router", 'ngMaterial', 'LocalStorageModule'])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('green', {
+                'default': '900',
+                'hue-1': '50',
+                'hue-2': '100',
+                'hue-3': '200'
+            })
+            .accentPalette('grey', {
+                'default': '900',
+                'hue-1': '50',
+                'hue-2': '100',
+                'hue-3': '200'
+            })
+            .warnPalette('red', {
+                'default': '900',
+                'hue-1': '50',
+                'hue-2': '100',
+                'hue-3': '200'
+            })
+    });
