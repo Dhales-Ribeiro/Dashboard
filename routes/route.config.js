44fa584c2957e3e4f05863eba850979d7c4a536c
Dhales Ribeiro <wm.dhales@gmail.com>
2016-11-27 08:41:46 -0200

    - configurando rotas do sistema

--- routes/route.config.js
+++ routes/route.config.js
@@ -6,9 +6,37 @@ application.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('login', {
             url: '/login',
-            templateUrl: './resources/views/auth/login.html',
-            controller: 'LoginController'
-        });
+            views: {
+                '': {
+                    templateUrl: './resources/views/auth/login.html',
+                    controller: 'LoginController'
+                }
+            }
+        })
+        .state('app', {
+            url: '/app',
+            views: {
+                '': {
+                    templateUrl: './resources/views/layout/app.layout.html'
+                },
+                'content': {
+                    templateUrl: './resources/views/partials/content.html',
+                    controller: 'appController'
+                },
+                'leftMenu': {
+                    templateUrl: './resources/views/partials/left.menu.html',
+                    controller: 'LeftMenuController'
+                },
+                'rightMenu': {
+                    template: 'menu-right'
+                }
+            }
+        })
+        .state('app.statistics', {
+            url: '/statistics',
+            templateUrl: './resources/views/statistics/statistics.html'
+        })
 
-    $urlRouterProvider.otherwise('/login');
+
+    $urlRouterProvider.otherwise('login');
 });
