bc047cb034ec4c4352a7e4d1128b658283f8b2a2
Dhales Ribeiro <wm.dhales@gmail.com>
2016-11-27 07:50:58 -0200

    - copiando novas libs

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,19 @@ var scripts = [
     vendors + 'bootstrap/dist/js/bootstrap.min.js',
     vendors + 'angular/angular.min.js',
     vendors + 'angular-ui-router/release/angular-ui-router.min.js',
+    vendors + 'angular-animate/angular-animate.min.js',
+    vendors + 'angular-aria/angular-aria.min.js',
+    vendors + 'angular-messages/angular-messages.min.js',
+    vendors + 'angular-material/angular-material.min.js',
+    vendors + 'angular-local-storage/dist/angular-local-storage.min.js',
+    vendors + 'particles.js/particles.js',
     './resources/assets/scripts/**',
     './config/**',
+    './app/dbService.js',
     './routes/**',
-    './app/**/**',
+    './app/Services/**',
+    './app/Constant/**.js',
+    './app/Controllers/**/**',
 ];
 
 var styles = [
@@ -21,8 +30,21 @@ var styles = [
     vendors + 'bootstrap/dist/css/bootstrap-theme.min.css',
 ];
 
+var fonts = [
+    vendors + 'material-design-icons/iconfont/**'
+];
+
+var images = [
+    './resources/assets/images/**'
+];
+
 elixier(function (mix) {
     mix.copy('./node_modules/jquery/dist/jquery.min.js', './public/js/');
+
+    mix.copy('./app/Constant/particles.json', './public/js/');
+    mix.copy(fonts, './public/fonts/');
+    mix.copy(images, './public/images/');
+
     mix.scripts(scripts);
     mix.styles(styles);
     
