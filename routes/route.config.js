dab3d8082dbb4d621add840f88526ae2516e8b33
Dhales Ribeiro <wm.dhales@gmail.com>
2016-11-28 21:09:35 -0200

    - configurando novas rotas do sistema para o admin

--- routes/route.config.js
+++ routes/route.config.js
@@ -38,5 +38,47 @@ application.config(function ($stateProvider, $urlRouterProvider) {
         })
 
 
+        // States For Administrator
+
+        .state('admLogin', {
+            url: '/adminLogin',
+            views: {
+                '': {
+                    templateUrl: './resources/views/auth/adm.login.html',
+                    controller: 'LoginController'
+                }
+            }
+        })
+
+        .state('adm', {
+            url: '/adm',
+            views: {
+                '': {
+                    templateUrl: './resources/views/layout/adm.layout.html'
+                },
+                'content': {
+                    templateUrl: './resources/views/partials/adm.content.html',
+                    controller: 'appController'
+                },
+                'leftMenu': {
+                    templateUrl: './resources/views/partials/admLeft.menu.html',
+                    controller: 'LeftMenuController'
+                },
+                'rightMenu': {
+                    template: 'menu-right'
+                }
+            }
+        })
+        .state('adm.statistics', {
+            url: '/adminStatistics',
+            templateUrl: './resources/views/statistics/statistics.html'
+        })
+        .state('adm.users', {
+            url: '/adminUsers',
+            templateUrl: './resources/views/users/index.html',
+            controller: 'userControler'
+        })
+
+
     $urlRouterProvider.otherwise('login');
 });
