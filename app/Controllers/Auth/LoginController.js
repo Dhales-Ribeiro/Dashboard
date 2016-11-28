92a4723a4ac10343d9e124f79fb4f29995525c49
Dhales Ribeiro <wm.dhales@gmail.com>
2016-11-28 21:01:59 -0200

    - fazendo autenticação para o painel administrativo

--- app/Controllers/Auth/LoginController.js
+++ app/Controllers/Auth/LoginController.js
@@ -2,8 +2,14 @@
  * Created by dhales on 20/11/16.
  */
 application.controller('LoginController', function ($scope, $location, $state, AuthenticateService, localStorageService) {
-    particlesJS.load('particles-js', './public/js/particles.json', function() {
-    });
+
+    if($state.current.url == '/login'){
+        particlesJS.load('particles-js', './public/js/particles.json', function() {
+        });
+    }
+
+    AuthenticateService.clear();
+
     $scope.login = function (user) {
         AuthenticateService.authenticate(user).then(function (res) {
             if(res.length == 1){
@@ -11,6 +17,22 @@ application.controller('LoginController', function ($scope, $location, $state, A
                 var data = res[0];
                 data._embedded;
                 localStorageService.set('userLogged', data);
+            }else{
+                console.log("erro")
+            }
+
+        }, function (error) {
+            console.log(error)
+        })
+    }
+
+    $scope.adminLogin = function (user) {
+        AuthenticateService.authenticate(user).then(function (res) {
+            if(res.length == 1 && res[0]['role_id'] == 1){
+                var data = res[0];
+                data._embedded;
+                localStorageService.set('admLogged', data);
+                $state.go('adm.statistics');
             }else{
                 console.log("erro")
             }
