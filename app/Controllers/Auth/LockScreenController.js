7a90002d0644f26ed2a66533a20d487aa697632d
Dhales Ribeiro <wm.dhales@gmail.com>
2016-12-04 14:48:57 -0200

    - criando verificaçao para adm

---
+++ app/Controllers/Auth/LockScreenController.js
@@ -0,0 +1,63 @@
+/**
+ * Created by dhales on 04/12/16.
+ */
+
+application.controller('LockScreenController', function ($scope, $location, $state, localStorageService, menssagesService) {
+    particlesJS.load('particles-js', './public/js/particles.json', function() {
+    });
+
+    var localStorageKey = localStorageService.keys();
+
+    if(localStorageKey == 'userLogged'){
+        var lock = localStorageService.get('userLogged');
+        if(angular.isObject(lock)){
+            $scope.lock = lock;
+        }else{
+            $scope.lock = {
+                name: 'Indefinido',
+                email: 'Indefinido'
+            };
+        }
+    }
+
+
+
+    if(localStorageKey == 'admLogged'){
+        var lock = localStorageService.get('admLogged');
+        if(angular.isObject(lock)){
+            $scope.lock = lock;
+        }else{
+            $scope.lock = {
+                name: 'Indefinido',
+                email: 'Indefinido',
+            };
+        }
+    }
+
+    $scope.enter = function (pass) {
+        if(pass == undefined){
+            var text = {
+                content: "Digite uma senha!",
+                action: "Fechar",
+                style: "bold md-warn"
+            }
+            menssagesService.openMenssage(text)
+            return false;
+        }
+
+        if(lock.password === pass.password){
+            $state.go('adm.dashboard');
+        }else{
+            var text = {
+                content: "Erro, senha invalida.",
+                action: "Fechar",
+                style: "bold md-warn"
+            }
+            menssagesService.openMenssage(text)
+        }
+    }
+
+
+
+
+})
