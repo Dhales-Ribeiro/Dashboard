e596b86e0212df7d1ac148ce328b73bb6d9cb275
Dhales Ribeiro <wm.dhales@gmail.com>
2016-12-10 10:35:47 -0200

    - finalizando serviços de perfil

--- app/Services/roleService.js
+++ app/Services/roleService.js
@@ -4,9 +4,19 @@
 
 application.factory('roleService', function (dbService,$q) {
 
-        var _getRole = function (role) {
+    var _getRole = function (role) {
         var deferred = $q.defer();
         var result = dbService.run("select r.id, r.rolename from roles r where r.rolename = '" + role + "';", function (res) {
+            if (res.error)
+                throw res.error
+            deferred.resolve(res);
+        })
+        return deferred.promise;
+    }
+
+    var _findRole = function (id) {
+        var deferred = $q.defer();
+        var result = dbService.run("select * from roles r where r.id = '" + id + "';", function (res) {
             if (res.error)
                 throw res.error
             deferred.resolve(res);
@@ -40,13 +50,38 @@ application.factory('roleService', function (dbService,$q) {
         return deferrend.promise;
     }
 
+    var _updadeRole = function (data) {
+        if(!angular.isObject(data)){
+            throw new  TypeError("Erro, Não é um Objeto para Update perfil");
+            return false;
+        }
+        var deferrend = $q.defer();
+        var result = dbService.run("update roles set slug='"+data.slug+"', rolename='"+data.role+"',  updated_at='"+moment().format('DD/MM/YYYY h:mm:ss a')+"' where id='"+data.id+"';", function (res) {
+            if(res.error)
+                throw res.error
+            deferrend.resolve(res)
+        })
+        return deferrend.promise;
+    }
 
+    var _deleteRole = function (id) {
+        var deferrend = $q.defer();
+        var result = dbService.run("delete from roles where id = '"+id+"';", function (res) {
+            if(res.error)
+                throw res.error
+            deferrend.resolve(res)
+        })
+        return deferrend.promise;
+    }
 
 
     return {
         getRole: _getRole,
+        findRole: _findRole,
         getAllRole: _getAllRole,
-        registerRole: _registerRole
+        registerRole: _registerRole,
+        updadeRole: _updadeRole,
+        deleteRole: _deleteRole
     }
-    
+
 })
